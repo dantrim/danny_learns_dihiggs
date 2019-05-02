a5e029fb24a23b608a6540c703113da3b4e83f75
Daniel Antrim <dantrim@uci.edu>
2019-05-02 00:49:59 -0700

    cleanup callbacks

--- wwbb_callbacks.py
+++ wwbb_callbacks.py
@@ -19,32 +19,20 @@ class roc_callback(Callback) :
     def on_epoch_end(self, epoch, logs = {}) :
 
         self.val_losses.append(logs.get('val_loss'))
-        #print('training data inputs (shape) : %s (%s)' % (list(self.model.inputs[0]), self.model.inputs[0].shape))
-        #print('training data outputs (shape): %s (%s)' % (list(self.model.targets[0]), self.model.targets[0].shape))
-        #self.train_losses.append(logs.get('loss'))
 
         y_pred = self.model.predict(self.validation_data[0])
         y_true = self.validation_data[1]
         val_auc = roc_auc_score(y_true, y_pred)
-
-        #y_train_pred = self.model.predict(self.train_data[0])
-        #y_train_true = self.train_data[1]
-        #train_auc = roc_auc_score(y_train_true, y_train_pred)
-        #print('train_roc_auc = %s, val_roc_auc = %s' % (str(round(train_auc,4), str(round(val_auc,4)))))
         print('val_roc_auc = %s' % ( str(round(val_auc,4)) ))
-
         self.val_aucs.append(val_auc)
-        #self.train_aucs.append(train_auc)
-        #self.aucs.append(auc)
         logs['val_roc_auc'] = self.val_aucs
-        #logs['train_roc_auc'] = self.train_aucs
 
         return
 
     def on_batch_begin(self, batch, logs = {}) :
         return
 
-def lr_step_decay(epoch) : #initial_lr, drop, epoch_to_drop) :
+def lr_step_decay(epoch) :
 
     initial_lr = 0.5
     drop = 0.9
@@ -54,18 +42,8 @@ def lr_step_decay(epoch) : #initial_lr, drop, epoch_to_drop) :
         epoch == 50
     elif epoch >= 25 :
         epoch -= 25
-#        new_lr = initial_lr
         print('INFO Setting learning rate back to initial LR (={})'.format(initial_lr))
 
     new_lr = initial_lr * math.pow(drop, math.floor((1+epoch)/epoch_to_drop))
     print('INFO LR Schedule: {}'.format(new_lr))
     return new_lr
-
-#def wwbb_lr_schedule(initial_lr = lr, epoch_to_reset = 50) :
-#
-#    def schedule(epoch) :
-#        if epoch == epoch_to_reset :
-#            return initial_lr
-#        else :
-#            return 
-#
