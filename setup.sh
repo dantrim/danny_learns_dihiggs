ca8e7c7b9ff68e5937aec72233780a4f116d923f
Benjamin Rottler <benjamin.rottler@cern.ch>
2019-05-02 14:28:55 +0200

    Fix setup for any machine with cvmfs

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 # call this script inside of the danny_learns_dihiggs/ directory
 
+setupATLAS
+
 export PYTHONPATH=${PWD}:${PYTHONPATH}
 export PYTHONPATH=${PWD}/analyze:${PYTHONPATH}
 lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt tensorflow" > /dev/null  # TF 1.8.0 and python3
+lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt h5py" > /dev/null
+lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt scikitlearn" > /dev/null
+lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras"  > /dev/null
+lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras_applications" > /dev/null
+lsetup "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras_preprocessing" > /dev/null
